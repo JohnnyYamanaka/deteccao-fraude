a97299b490dfccb4633124f8ce2dc80303c3ad95
Johnny Yamanaka <yamanaka.johnny@outlook.com>
2022-06-12 22:01:09 -0300

    criando webapp com streamlit

--- helpers/evaluate.py
+++ helpers/evaluate.py
@@ -2,6 +2,14 @@ import pandas as pd
 from sklearn.metrics import classification_report, roc_auc_score
 from sklearn.model_selection import train_test_split
 
+#Pre processamento
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
+#Pipeline
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
 class EvaluateModel():
     def __init__(self, model, dataset: pd.DataFrame()):
         self.model = model
@@ -35,3 +43,37 @@ class EvaluateModel():
         print(f'Total of loss: ${df_evaluate["financialLost"].sum(): ,.2f}')
 
         return fited_model
+
+    def predict(self):
+        X = self.dataset.drop('isFraud', axis=1).copy()
+        y = self.dataset['isFraud']
+
+        cat_features = self.dataset.select_dtypes(include='object').copy().columns
+        cat_features = cat_features.append(
+            pd.Index(['higherValue', 'higherOldBalance', 'withdrawAll'])
+        )
+
+        numeric_features = self.dataset.select_dtypes(include='number').copy().columns
+        numeric_features = numeric_features.delete([-3, -4, -5, -7])
+
+        onehot = OneHotEncoder()
+        standard = StandardScaler()
+
+        preprocessor = ColumnTransformer(transformers=[
+        ('one', onehot, cat_features),
+        ('standard', standard, numeric_features)])
+
+        model = self.model
+
+        pred = model.predict(X)
+        roc_auc = roc_auc_score(y, pred)
+
+        df_evaluate = self.dataset.copy()
+        df_evaluate['predicted'] = pred
+
+        df_evaluate['financialLost'] = df_evaluate.apply(lambda x: -x['amount'] if 
+            ((x['isFraud'] == 1) and (x['predicted'] == 0)) or
+            ((x['isFraud'] == 0) and (x['predicted'] == 1))
+            else 0, axis=1)
+
+        return roc_auc, df_evaluate, df_evaluate['financialLost'].sum()
